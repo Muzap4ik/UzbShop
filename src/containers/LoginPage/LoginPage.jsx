50c9b0ad0c1fde09d44a0a5f884937dde6d86047
unknown <muzapparaxi@gmail.com>
2023-06-19 15:17:22 +0500

    changed window location on LoginPage to home

--- src/containers/LoginPage/LoginPage.jsx
+++ src/containers/LoginPage/LoginPage.jsx
@@ -80,7 +80,7 @@ const LoginPage = () => {
     const submitHandler = (e) => {
         e.preventDefault()
         console.log(`Ismingiz ${name}\nPochtangiz: ${email}\nYoshingiz: ${age}`)
-        /* window.location.replace("https://zhamal2k04.github.io/Project--Designo#/home") */
+        window.location.replace("htpps://Muzap4ik.github.io/UzbShop/#/home")
     }
 
   return (
